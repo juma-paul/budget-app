7d2666fea7d127e86bb3292b2fe216111a8fa3b0
juma <raisben7@gmail.com>
2025-06-24 18:22:32 -0700

    add function for generating refresh tokens

--- src/modules/authentication.ts
+++ src/modules/authentication.ts
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import { createAccessToken, setAccessTokenCookie } from "../utils/tokens.js";
 
 export const comparePasswords = (password, hashed) => {
   return bcrypt.compare(password, hashed);
@@ -9,20 +10,8 @@ export const hashPassword = (password) => {
   return bcrypt.hash(password, 12);
 };
 
-export const createJwtToken = (user) => {
-  const token = jwt.sign(
-    {
-      id: user.id,
-      username: user.username,
-    },
-    process.env.JWT_SECRET
-  );
-
-  return token;
-};
-
 export const protect = (req, res, next) => {
-  const token = req.cookies.token;
+  const token = req.cookies.accessToken;
 
   // check if a token is provided
   if (!token) {
@@ -38,5 +27,24 @@ export const protect = (req, res, next) => {
   } catch (err) {
     console.error(err);
     res.status(401).json({ error: "Not a valid token!" });
+  }
+};
+
+// Generate new access token
+export const freshAccessToken = (req, res) => {
+  const token = req.cookies.refreshToken;
+
+  // Check if client has a refresh token
+  if (!token) {
+    return res.status(401).json({ error: "No refresh token." });
+  }
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+    const newAccessToken = createAccessToken(user);
+    setAccessTokenCookie(res, newAccessToken);
+    res.status(200).json({ success: "Access token refreshed." });
+  } catch (err) {
+    res.status(403).json({ error: "Invalid refresh token." });
   }
 };
